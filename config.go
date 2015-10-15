50f7584e1c7c0717e399652d1742f8b499eef860
Robert Gerus <arachnist@i.am-a.cat>
2015-10-15 16:50:32 +0200

    Some stubs and a possibly working config class.

---
+++ config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+    "encoding/json"
+    "io/ioutil"
+)
+
+type Config struct {
+    Nick        string
+    Host        string
+    Networks    []string
+    Servers     map[string] string
+    Channels    map[string] string
+    Passwords   map[string] string
+}
+
+func ReadConfig(path string) (Config, error) {
+    var config Config
+    
+    data, err := ioutil.ReadFile(path)
+    if err != nil {
+        return config, err
+    }
+
+    err = json.Unmarshal(data, &config)
+    if err != nil {
+        return config, err
+    }
+
+    return config, nil
+}
