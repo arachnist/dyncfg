65d5e7cc48499dacdd8f43021ccc012aaf8c3432
Robert Gerus <arachnist@i.am-a.cat>
2015-10-20 10:59:13 +0200

    Expose username and realname to configuration and make use of it in irc.Dial()

--- config.go
+++ config.go
@@ -1,35 +1,36 @@
 package config
 
 import (
-    "encoding/json"
-    "io/ioutil"
+	"encoding/json"
+	"io/ioutil"
 )
 
 type Config struct {
-    Nick        string
-    Host        string
-    RealName    string
-    Networks    []string
-    Servers     map[string] []string
-    Channels    map[string] []string
-    Passwords   map[string] string
-    Plugins     []string
-    Ignore      []string
-    Logpath     string
+	Nick      string
+	Host      string
+	RealName  string
+	User      string
+	Networks  []string
+	Servers   map[string][]string
+	Channels  map[string][]string
+	Passwords map[string]string
+	Plugins   []string
+	Ignore    []string
+	Logpath   string
 }
 
 func ReadConfig(path string) (Config, error) {
-    var config Config
-    
-    data, err := ioutil.ReadFile(path)
-    if err != nil {
-        return config, err
-    }
+	var config Config
 
-    err = json.Unmarshal(data, &config)
-    if err != nil {
-        return config, err
-    }
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return config, err
+	}
 
-    return config, nil
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
 }
