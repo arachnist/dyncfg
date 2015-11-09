d06cbfb926b7a7efd0825acec1457c3ba8aad5d0
Robert Gerus <rgerus@dpd.com.pl>
2015-11-09 14:32:26 +0100

    It's hierryfying and does not build… yet

--- config.go
+++ config.go
@@ -3,6 +3,9 @@ package config
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
+	"os"
+	"path"
 )
 
 type Config struct {
@@ -17,20 +20,51 @@ type Config struct {
 	Plugins   []string
 	Ignore    []string
 	Logpath   string
+	ConfigDir string
 }
 
-func ReadConfig(path string) (Config, error) {
-	var config Config
+type Context struct {
+	Network string
+	Target  string
+	Source  string
+}
+
+var C Config
+
+func lookupvar(key, path string) interface{} {
+	var f interface{}
+	_, err := os.Stat(path)
+	if err != nil {
+		return ""
+	}
 
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		return config, err
+		return ""
 	}
 
-	err = json.Unmarshal(data, &config)
-	if err != nil {
-		return config, err
-	}
+	err = json.Unmarshal(data, &f)
 
-	return config, nil
+	return f.(map[string]interface{})[key]
+}
+
+func (c *Config) Lookup(context Context, key string) interface{} {
+	var fpath string
+	var value interface{}
+	if context.Network != "" {
+		if context.Source != "" {
+			if context.Target != "" {
+				fpath = path.Join(c.ConfigDir, context.Network, context.Source, context.Target+".json")
+
+				log.Println("Context:", context, "Looking up", key, "in", fpath)
+				value = lookupvar(key, fpath)
+				if value != nil {
+					return value
+				}
+			}
+			return lookupvar(key, path.Join(c.ConfigDir, context.Network, context.Source+".json"))
+		}
+		return lookupvar(key, path.Join(c.ConfigDir, context.Network+".json"))
+	}
+	return lookupvar(key, path.Join(c.ConfigDir, "common.json"))
 }
