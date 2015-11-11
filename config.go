38c24a81d64704ab347399042582842a78cf27eb
Robert Gerus <arachnist@i.am-a.cat>
2015-11-11 21:38:33 +0100

    Code cleanup - do away with exports

--- config.go
+++ config.go
@@ -9,38 +9,38 @@ import (
 )
 
 type cacheEntry struct {
-	ModTime  time.Time
-	Contents interface{}
+	modTime  time.Time
+	contents interface{}
 }
 
-type Config struct {
-	Cache         map[string]cacheEntry
-	BuildFileList func(map[string]string) []string
+type config struct {
+	cache         map[string]cacheEntry
+	buildFileList func(map[string]string) []string
 }
 
-var C Config
+var c config
 
 func init() {
 	log.Println("Initializing configuration cache")
-	C.Cache = make(map[string]cacheEntry)
+	c.cache = make(map[string]cacheEntry)
 }
 
 func lookupvar(key, path string) interface{} {
 	var f interface{}
 	i, err := os.Stat(path)
-	_, ok := C.Cache[path]
+	_, ok := c.cache[path]
 	if os.IsNotExist(err) {
 		log.Println("Config does not exist", path)
 		if ok {
 			log.Println("Purging", path, "from cache")
-			delete(C.Cache, path)
+			delete(c.cache, path)
 		}
 	}
 	if err != nil {
 		return nil
 	}
 
-	if C.Cache[path].ModTime.Before(i.ModTime()) || !ok {
+	if c.cache[path].modTime.Before(i.ModTime()) || !ok {
 		log.Println("Stale cache for", path)
 		data, err := ioutil.ReadFile(path)
 		if err != nil {
@@ -54,22 +54,26 @@ func lookupvar(key, path string) interface{} {
 		}
 
 		log.Println("Updating cache for", path)
-		C.Cache[path] = cacheEntry{
-			ModTime:  i.ModTime(),
-			Contents: f,
+		c.cache[path] = cacheEntry{
+			modTime:  i.ModTime(),
+			contents: f,
 		}
 
 		return f.(map[string]interface{})[key]
-	} else {
-		log.Println("Found cache for", path)
-		return C.Cache[path].Contents.(map[string]interface{})[key]
 	}
+
+	log.Println("Found cache for", path)
+	return c.cache[path].contents.(map[string]interface{})[key]
 }
 
-func (c *Config) Lookup(context map[string]string, key string) interface{} {
+func SetFileListBuilder(f func(map[string]string) []string) {
+	c.buildFileList = f
+}
+
+func Lookup(context map[string]string, key string) interface{} {
 	var value interface{}
 
-	for _, fpath := range c.BuildFileList(context) {
+	for _, fpath := range c.buildFileList(context) {
 		log.Println("Context", context, "Looking up", key, "in", fpath)
 		value = lookupvar(key, fpath)
 		if value != nil {
