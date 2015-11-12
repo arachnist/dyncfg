5a5b86069eb47bec504f8f1bc94871c3b0518628
Robert Gerus <rgerus@dpd.com.pl>
2015-11-12 12:55:51 +0100

    Documentation/comment/readme fixes here and there.

--- config.go
+++ config.go
@@ -1,6 +1,8 @@
 // Copyright 2015 Robert S. Gerus. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
+
+// Package config is a hiera-inspired configuration library.
 package config
 
 import (
