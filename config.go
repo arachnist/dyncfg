d7a3138077ccd89625fee161475f81bdfe512528
Robert Gerus <arachnist@i.am-a.cat>
2015-11-16 20:25:18 +0100

    Fix godoc a bit.

--- config.go
+++ config.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-// Package config is a hiera-inspired configuration library.
 package config
 
 import (
