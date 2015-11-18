cdeefaf62298171f4fc5727d7e6453c7f3fd052c
Robert Gerus <rgerus@dpd.com.pl>
2015-11-18 19:07:08 +0100

    Test suite for config package.

---
+++ config1.2_test.go
@@ -0,0 +1,148 @@
+// Copyright 2015 Robert S. Gerus. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// +build go1.2,!go1.4
+
+package config
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"reflect"
+	"testing"
+)
+
+var emptyContext map[string]string
+
+type testElem struct {
+	key           string
+	expectedValue interface{}
+}
+
+// func Lookup(context map[string]string, key string) interface{}
+var testsLookup = []struct {
+	key           string
+	expectedValue interface{}
+}{
+	{
+		key:           "TestLookup",
+		expectedValue: "this can be anything you can imagine, but now it's just a string",
+	},
+	{
+		key:           "NotExisting",
+		expectedValue: nil,
+	},
+}
+
+func TestLookup(t *testing.T) {
+	for i, e := range testsLookup {
+		v := Lookup(emptyContext, e.key)
+		if fmt.Sprintf("%+v", v) != fmt.Sprintf("%+v", e.expectedValue) {
+			t.Log("test#", i, "Lookup key", e.key)
+			t.Logf("expected: %+v\n", e.expectedValue)
+			t.Logf("result  : %+v\n", v)
+			t.Fail()
+		}
+	}
+}
+
+// func LookupString(context map[string]string, key string) string
+var testsLookupString = []struct {
+	key           string
+	expectedValue string
+}{
+	{
+		key:           "TestLookupString",
+		expectedValue: "this should be a string",
+	},
+	{
+		key:           "NotExisting",
+		expectedValue: "",
+	},
+}
+
+func TestLookupString(t *testing.T) {
+	for i, e := range testsLookupString {
+		v := LookupString(emptyContext, e.key)
+		if fmt.Sprintf("%+v", v) != fmt.Sprintf("%+v", e.expectedValue) {
+			t.Log("test#", i+1, "Lookup key", e.key)
+			t.Logf("expected: %+v\n", e.expectedValue)
+			t.Logf("result  : %+v\n", v)
+			t.Fail()
+		}
+	}
+}
+
+// func LookupStringSlice(context map[string]string, key string) []string
+var testsLookupStringSlice = []struct {
+	key           string
+	expectedValue []string
+}{
+	{
+		// TestLookupStringSlice return slice is ordered
+		key:           "TestLookupStringSlice",
+		expectedValue: []string{"a", "am", "be", "going", "i", "slice", "to"},
+	},
+	{
+		key:           "NotExisting",
+		expectedValue: []string{""},
+	},
+}
+
+func TestLookupStringSlice(t *testing.T) {
+	for i, e := range testsLookupStringSlice {
+		v := LookupStringSlice(emptyContext, e.key)
+		if fmt.Sprintf("%+v", v) != fmt.Sprintf("%+v", e.expectedValue) {
+			t.Log("test#", i+1, "Lookup key", e.key)
+			t.Logf("expected: %+v\n", e.expectedValue)
+			t.Logf("result  : %+v\n", v)
+			t.Fail()
+		}
+	}
+}
+
+// func LookupStringMap(context map[string]string, key string) map[string]bool
+var testsLookupStringMap = []struct {
+	key           string
+	expectedValue map[string]bool
+}{
+	{
+		key: "TestLookupStringMap",
+		expectedValue: map[string]bool{
+			"to":              true,
+			"be":              true,
+			"a":               true,
+			"map[string]bool": true,
+			"I":               true,
+			"want":            true,
+		},
+	},
+	{
+		key:           "NotExisting",
+		expectedValue: map[string]bool{},
+	},
+}
+
+func TestLookupStringMap(t *testing.T) {
+	for i, e := range testsLookupStringMap {
+		v := LookupStringMap(emptyContext, e.key)
+		if !reflect.DeepEqual(v, e.expectedValue) {
+			t.Log("test#", i+1, "Lookup key", e.key)
+			t.Logf("expected: %+v\n", e.expectedValue)
+			t.Logf("result  : %+v\n", v)
+			t.Fail()
+		}
+	}
+}
+
+func configLookupHelper(map[string]string) []string {
+	return []string{"this/thing/does/not/exist.json", ".testconfig.json"}
+}
+
+func init() {
+	SetFileListBuilder(configLookupHelper)
+	log.SetOutput(ioutil.Discard)
+}
